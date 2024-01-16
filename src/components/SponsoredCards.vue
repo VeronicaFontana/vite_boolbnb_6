511c98bec22883cb99de0636356a8bb32559f9c0
alessandrodaniello105 <kanymsevement@gmail.com>
2024-01-16 19:23:36 +0100

    [add]: SponsoredCards as component; [test] api calls backend

---
+++ src/components/SponsoredCards.vue
@@ -0,0 +1,58 @@
+<script>
+  import axios from 'axios';
+  import {store} from '../data/store.js'
+  import Card from './partials/Card.vue';
+
+
+  export default {
+
+    name: 'SponsoredCards',
+
+    components: {
+      Card
+    },
+
+    data() {
+      return {
+        store
+      }
+    },
+
+    methods: {
+      getApartments() {
+        axios.get(store.apiUrl, {
+          params: {
+          }
+        })
+        .then((res) => {
+          store.apartments = res.data.apartments;
+          console.log(res.data.apartments);
+        })
+        .catch(function (error) {
+          // handle error
+          console.log(error);
+        })
+      }
+    },
+    computed: {},
+    mounted() {
+      this.getApartments();
+    }
+    
+  }
+
+</script>
+
+<template>
+
+  <div class="border-1 ">
+    <h2>test SponsoredCards</h2>
+
+    <Card v-for="apartment in store.apartments" :key="apartment.id" :apartment="apartment" />
+  </div>
+
+</template>
+
+<style>
+
+</style>
