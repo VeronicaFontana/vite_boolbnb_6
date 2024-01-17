3e7772edb45b9e1eada663ccbac6e1423ba3e8e5
vincenzoimmordino92 <vincenzoimmordino92@gmail.com>
2024-01-17 14:35:12 +0100

    [add] AdvanceSearch vue

---
+++ src/pages/AdvanceSearch.vue
@@ -0,0 +1,25 @@
+<script>
+import Header from '../components/partials/Header.vue';
+import Footer from '../components/partials/Footer.vue';
+
+  export default {
+  name:'AdvanceSearch',
+  data(){
+    return{}
+  },
+  components:{
+    Header,
+    Footer
+  },
+  methods:{},
+  mounted(){},
+  computed:{}
+  }
+  </script>
+
+
+<template>
+  
+  <Jumbotron />
+  <Footer />
+</template>
