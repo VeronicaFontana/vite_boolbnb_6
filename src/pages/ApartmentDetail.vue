4de8292c6a4ca2df3127f1aa9d7e316a988a92bb
vincenzoimmordino92 <vincenzoimmordino92@gmail.com>
2024-01-19 15:51:33 +0100

    [add] ApartmentDetail

---
+++ src/pages/ApartmentDetail.vue
@@ -0,0 +1,35 @@
+<script>
+import {store} from '../data/store';
+import Header from '../components/partials/Header.vue';
+import Footer from '../components/partials/Footer.vue';
+
+  export default {
+  name:'ApartmentDetail',
+  data(){
+    return{
+      store
+    }
+  },
+  components:{
+    Header,
+    Footer
+  },
+  methods:{
+    
+  },
+  mounted(){},
+  computed:{}
+  }
+  </script>
+
+
+<template>
+  <Header />
+  <SearchBar />
+  <Results />
+  <Footer />
+</template>
+
+<style lang="scss" scoped>
+
+</style>
