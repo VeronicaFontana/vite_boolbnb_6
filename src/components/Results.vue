e8873e01fc66be73b276391b670ec0105f9c822a
vincenzoimmordino92 <vincenzoimmordino92@gmail.com>
2024-01-19 13:54:58 +0100

    [edit] delete Card + add CardResult in AdvanceSearch

--- src/components/Results.vue
+++ src/components/Results.vue
@@ -1,52 +1,30 @@
 <script>
-
+import { store } from '../data/store';
+import CardResult from './partials/CardResult.vue';
   export default {
 
     name: 'Results',
 
     data() {
-      return {}
+      return {
+        store
+      }
     },
-
+    components:{
+      CardResult
+    },
     methods: {},
     computed: {},
-    mounted() {}
+    mounted() {
+
+    }
     
   }
 
 </script>
 
 <template>
-
-<section class="results-box">
-  <div class="card mb-3" style="max-width: 540px;">
-    <div class="row g-0">
-      <div class="col-md-4">
-        <img src="..." class="img-fluid rounded-start" alt="...">
-      </div>
-      <div class="col-md-8">
-        <div class="card-body">
-          <h5 class="card-title">Nome casa</h5>
-          <div class="row w-75 h-50">
-            <div class="col-3">
-              <span><i class="fa-solid fa-person-shelter"></i> 2</span><br>
-              <span>locali</span>
-            </div>
-            <div class="col-4">
-              <span><i class="fa-solid fa-ruler-combined"></i> 50m²</span><br>
-              <span>superficie</span>
-            </div>
-            <div class="col-3">
-              <span><i class="fa-solid fa-restroom"></i> 2</span><br>
-              <span>bagni</span>
-            </div>
-          </div>
-          <p class="card-text">Descrizione</p>
-        </div>
-      </div>
-    </div>
-  </div>
-</section>
+<CardResult v-for="result in store.results" :key="result.id" :result="result"/>
   
 </template>
 
