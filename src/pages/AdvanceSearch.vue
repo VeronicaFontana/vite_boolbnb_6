ef412864ffbf878a50e4f787cd480485d1cdb030
VeronicaFontana <fontana.veronica98@gmail.com>
2024-01-19 18:38:39 +0100

    [add] Advance Search filter box

--- src/pages/AdvanceSearch.vue
+++ src/pages/AdvanceSearch.vue
@@ -30,9 +30,84 @@ import Results from '../components/Results.vue';
 <template>
   <Header />
   <SearchBar />
+
+  <section class="filter-box">
+    <div class="ms-2 h-100 d-flex align-items-center">
+
+      <!-- STANZE -->
+      <div class="dropdown">
+        <button class="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+          Stanze
+        </button>
+        <ul class="dropdown-menu">
+          <li><a class="dropdown-item" >Action</a></li>
+          <li><a class="dropdown-item" >Another action</a></li>
+          <li><a class="dropdown-item" >Something else here</a></li>
+        </ul>
+      </div>
+
+      <!-- BAGNI -->
+      <div class="dropdown">
+        <button class="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+          Bagni
+        </button>
+        <ul class="dropdown-menu">
+          <li><a class="dropdown-item" >Action</a></li>
+          <li><a class="dropdown-item" >Another action</a></li>
+          <li><a class="dropdown-item" >Something else here</a></li>
+        </ul>
+      </div>
+
+      <!-- CAMERE -->
+      <div class="dropdown">
+        <button class="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+          Camere da letto
+        </button>
+        <ul class="dropdown-menu">
+          <li><a class="dropdown-item" >Action</a></li>
+          <li><a class="dropdown-item" >Another action</a></li>
+          <li><a class="dropdown-item" >Something else here</a></li>
+        </ul>
+      </div>
+
+      <!-- SUPERFICIE -->
+      <div class="dropdown">
+        <button class="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+          Superficie
+        </button>
+        <ul class="dropdown-menu">
+          <li><a class="dropdown-item" >Action</a></li>
+          <li><a class="dropdown-item" >Another action</a></li>
+          <li><a class="dropdown-item" >Something else here</a></li>
+        </ul>
+      </div>
+
+      <!-- SERVIZI -->
+      <div class="dropdown">
+        <button class="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+          Servizi
+        </button>
+        <ul class="dropdown-menu">
+          <li><a class="dropdown-item" ><span class="badge text-bg-secondary">Secondary</span></a></li>
+        </ul>
+      </div>
+
+    </div>
+  </section>
+
   <Results />
   <Footer />
 </template>
 
 <style lang="scss" scoped>
+.filter-box{
+  width: 100vw;
+  height: 60px;
+  background-color: lightgrey;
+
+  .dropdown{
+    margin-right: 20px;
+  }
+}
+
 </style>
