2963420869bdd6b78d29c35f2034aa3b9d8a386f
vincenzoimmordino92 <vincenzoimmordino92@gmail.com>
2024-01-19 15:50:49 +0100

    [fix] box_cardResult

--- src/components/Results.vue
+++ src/components/Results.vue
@@ -24,23 +24,17 @@ import CardResult from './partials/CardResult.vue';
 </script>
 
 <template>
-<CardResult v-for="result in store.results" :key="result.id" :result="result"/>
+  <div class="box_cardResult">
+    <CardResult v-for="result in store.results" :key="result.id" :result="result"/>
+
+  </div>
   
 </template>
 
 <style lang="scss" scoped>
-
-.results-box{
-  margin: 20px;
-
-  .row{
-    padding: 0px 0px 9px 12px;
-    .col-3, .col-4{
-      padding: 0;
-      font-size: .6rem;
-      color: grey;
-    }
-  }
+.box_cardResult{
+  display: flex;
+  flex-wrap: wrap;
 }
 
 </style>
