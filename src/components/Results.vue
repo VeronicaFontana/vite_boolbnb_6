7bff8f450ff7fff5facfb38b442e341386bf6869
vincenzoimmordino92 <vincenzoimmordino92@gmail.com>
2024-01-20 13:13:24 +0100

    [add] animated text in Results if no results

--- src/components/Results.vue
+++ src/components/Results.vue
@@ -26,15 +26,34 @@ import CardResult from './partials/CardResult.vue';
 <template>
   <div class="box_cardResult">
     <CardResult v-for="result in store.results" :key="result.id" :result="result"/>
-
+    
   </div>
+  <h2 class="text-center " v-if="store.results.length == 0" >Non abbiamo nessun appartamento che soddisfi i tuoi criteri di ricerca &#9785;</h2>
   
 </template>
 
 <style lang="scss" scoped>
 .box_cardResult{
-  display: flex;
-  flex-wrap: wrap;
+  // display: flex;
+  // flex-wrap: wrap;
+  height:calc(100vh - 160px - 250px);
+  position: relative;
+  z-index:3;
+  
 }
+h2{
+    position: absolute;
+    bottom:100%;
+    z-index: 1;
+    animation-name: testo;
+    animation-duration: 2s;
+    animation-fill-mode: forwards;
+    left: 50%;
+    transform:translateX(-50%) ;
+  }
+  @keyframes testo{
+    0% {bottom:100%;}
+    100% {bottom:50%;}
+  }
 
 </style>
