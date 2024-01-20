550f31c88b583ec03eb8ff1cbef4e8ccb4a1c8b0
vincenzoimmordino92 <vincenzoimmordino92@gmail.com>
2024-01-20 14:27:11 +0100

    [add] get API singleApartment for ApartmentDeteail in store

--- src/data/store.js
+++ src/data/store.js
@@ -6,6 +6,7 @@ export const store = reactive({
   addressApiUrl: 'https://api.tomtom.com/search/2/geocode/',
   apiKey: 'H9LpZxM6CXyvTcM6MQohPzIS3ZH9d3Ke',
   apiNostra:'http://127.0.0.1:8000/api/search/',
+  apiSingleAparment:'http://127.0.0.1:8000/api/singleApartment/',
   apartments: [],
   addresses: [],
   sponsoredApartments: [],
