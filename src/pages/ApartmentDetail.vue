0f050cebba955cdbdf67315a173dc8e559c6b77f
vincenzoimmordino92 <vincenzoimmordino92@gmail.com>
2024-01-20 14:26:21 +0100

    [test] ApartmentDetail style

--- src/pages/ApartmentDetail.vue
+++ src/pages/ApartmentDetail.vue
@@ -20,10 +20,42 @@ import {store} from '../data/store';
 
 
 <template>
-  <SearchBar />
-  <Results />
+  <div class="box_detailapartment">
+<div class="row">
+
+  <div class="col-10 offset-1 border-2">
+    <div class="row">
+      <div class="col-8 vedo">
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint maxime sit cupiditate necessitatibus consequatur dignissimos minima explicabo quod, nesciunt voluptas quae vitae nulla dolor praesentium modi quas velit omnis ipsam eum. Quam, alias magnam, placeat iste laudantium totam nam at corporis nisi ducimus delectus magni. Porro officia cumque officiis consectetur?</p>
+    </div>
+
+    <div class="col-4">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Commodi minima dolor et eum nesciunt perspiciatis, optio aspernatur alias? Accusantium numquam nesciunt delectus ut incidunt aut debitis nostrum alias eligendi. Placeat adipisci vitae pariatur nostrum, inventore laudantium, corporis quae, odit odio magni vel maxime perspiciatis nisi modi obcaecati culpa quos quasi!
+    </div>
+    </div>
+  </div>
+</div>
+  </div>
 </template>
 
 <style lang="scss" scoped>
+.box_detailapartment{
+  margin-top: 90px;
+  .col-10{
+    border: solid 1px black;
+    background-color: brown;
+    .row{
+      .col-8{
+      background-color: darkcyan;
+      border: solid 1px rgb(182, 26, 26);
+      
 
+      }
+      .col-4{
+        background-color: darkorange;
+      border: solid 1px rgb(15, 202, 56);
+
+      }
+    }
+  }
+}
 </style>
