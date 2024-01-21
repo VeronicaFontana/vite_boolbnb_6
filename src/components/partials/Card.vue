09ef37c90a3131df6e31fd15e082c6c10fddd6f4
vincenzoimmordino92 <vincenzoimmordino92@gmail.com>
2024-01-21 16:43:17 +0100

    [fix] image src in Card

--- src/components/partials/Card.vue
+++ src/components/partials/Card.vue
@@ -18,7 +18,9 @@
 
 <template>
   <div class="card m-3" style="width: 18rem;">
-  <img :src="`http://127.0.0.1:8000/storage/`+apartment.image" class="card-img-top" alt="...">
+  <img v-if="apartment.image"
+            :src="`http://127.0.0.1:8000/storage/${apartment.image}`"
+            onerror="this.src='/Placeholder.png'" class="card-img-top" alt="...">
   <div class="card-body">
     <h5 class="card-title">{{apartment.title}}</h5>
   </div>
