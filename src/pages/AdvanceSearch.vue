3eb0aef0c1e502787170a5c919c4ceb100a6f062
VeronicaFontana <fontana.veronica98@gmail.com>
2024-01-22 12:58:42 +0100

    [edit] Filters dropdown in AdvanceSearch page

--- src/pages/AdvanceSearch.vue
+++ src/pages/AdvanceSearch.vue
@@ -29,59 +29,191 @@ import Results from '../components/Results.vue';
       <!-- STANZE -->
       <div class="dropdown">
         <button class="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
-          Stanze
+          <i class="fa-solid fa-person-shelter"></i> Stanze
         </button>
-        <ul class="dropdown-menu">
-          <li><a class="dropdown-item" >Action</a></li>
-          <li><a class="dropdown-item" >Another action</a></li>
-          <li><a class="dropdown-item" >Something else here</a></li>
+        <ul class="dropdown-menu p-0">
+          <li>
+            <a class="dropdown-item" >
+              <div class="p-3">
+                <input type="text" placeholder="Da" class="mx-2">
+                <input type="text" placeholder="A">
+              </div>
+            </a>
+          </li>
+          <li>
+            <a class="dropdown-item num">
+              Indifferente 
+            </a>
+            <a class="dropdown-item num">
+              1  
+            </a>
+            <a class="dropdown-item num">
+              2
+            </a>
+            <a class="dropdown-item num">
+              3 
+            </a>
+            <a class="dropdown-item num">
+              4  
+            </a>
+            <a class="dropdown-item num">
+              5+ 
+            </a>
+          </li>
+          <li>
+            <a class="dropdown-item" >
+              <div class="p-3 d-flex justify-content-center">
+                <button type="button" class="btn btn-danger">Cerca</button>
+              </div>
+            </a>
+          </li>
         </ul>
       </div>
 
       <!-- BAGNI -->
       <div class="dropdown">
         <button class="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
-          Bagni
+          <i class="fa-solid fa-restroom"></i> Bagni
         </button>
-        <ul class="dropdown-menu">
-          <li><a class="dropdown-item" >Action</a></li>
-          <li><a class="dropdown-item" >Another action</a></li>
-          <li><a class="dropdown-item" >Something else here</a></li>
+        <ul class="dropdown-menu p-0">
+          <li>
+            <a class="dropdown-item" >
+              <div class="p-3">
+                <input type="text" placeholder="Da" class="mx-2">
+                <input type="text" placeholder="A">
+              </div>
+            </a>
+          </li>
+          <li>
+            <a class="dropdown-item num">
+              Indifferente 
+            </a>
+            <a class="dropdown-item num">
+              1  
+            </a>
+            <a class="dropdown-item num">
+              2
+            </a>
+            <a class="dropdown-item num">
+              3 
+            </a>
+            <a class="dropdown-item num">
+              4  
+            </a>
+            <a class="dropdown-item num">
+              5+ 
+            </a>
+          </li>
+          <li>
+            <a class="dropdown-item" >
+              <div class="p-3 d-flex justify-content-center">
+                <button type="button" class="btn btn-danger">Cerca</button>
+              </div>
+            </a>
+          </li>
         </ul>
       </div>
 
       <!-- CAMERE -->
       <div class="dropdown">
         <button class="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
-          Camere da letto
+          <i class="fa-solid fa-bed"></i> Camere da letto
         </button>
-        <ul class="dropdown-menu">
-          <li><a class="dropdown-item" >Action</a></li>
-          <li><a class="dropdown-item" >Another action</a></li>
-          <li><a class="dropdown-item" >Something else here</a></li>
+        <ul class="dropdown-menu p-0">
+          <li>
+            <a class="dropdown-item" >
+              <div class="p-3">
+                <input type="text" placeholder="Da" class="mx-2">
+                <input type="text" placeholder="A">
+              </div>
+            </a>
+          </li>
+          <li>
+            <a class="dropdown-item num">
+              Indifferente 
+            </a>
+            <a class="dropdown-item num">
+              1  
+            </a>
+            <a class="dropdown-item num">
+              2
+            </a>
+            <a class="dropdown-item num">
+              3 
+            </a>
+            <a class="dropdown-item num">
+              4  
+            </a>
+            <a class="dropdown-item num">
+              5+ 
+            </a>
+          </li>
+          <li>
+            <a class="dropdown-item" >
+              <div class="p-3 d-flex justify-content-center">
+                <button type="button" class="btn btn-danger">Cerca</button>
+              </div>
+            </a>
+          </li>
         </ul>
       </div>
 
       <!-- SUPERFICIE -->
       <div class="dropdown">
         <button class="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
-          Superficie
+          <i class="fa-solid fa-ruler-combined"></i> Superficie
         </button>
-        <ul class="dropdown-menu">
-          <li><a class="dropdown-item" >Action</a></li>
-          <li><a class="dropdown-item" >Another action</a></li>
-          <li><a class="dropdown-item" >Something else here</a></li>
+        <ul class="dropdown-menu p-0">
+          <li>
+            <a class="dropdown-item" >
+              <div class="p-3">
+                <input type="text" placeholder="Da" class="mx-2">
+                <input type="text" placeholder="A">
+              </div>
+            </a>
+          </li>
+          <li>
+            <a class="dropdown-item num">
+              Indifferente 
+            </a>
+            <a class="dropdown-item num">
+              1  
+            </a>
+            <a class="dropdown-item num">
+              2
+            </a>
+            <a class="dropdown-item num">
+              3 
+            </a>
+            <a class="dropdown-item num">
+              4  
+            </a>
+            <a class="dropdown-item num">
+              5+ 
+            </a>
+          </li>
+          <li>
+            <a class="dropdown-item" >
+              <div class="p-3 d-flex justify-content-center">
+                <button type="button" class="btn btn-danger">Cerca</button>
+              </div>
+            </a>
+          </li>
         </ul>
       </div>
 
       <!-- SERVIZI -->
       <div class="dropdown">
         <button class="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
-          Servizi
+          <i class="fa-solid fa-bell-concierge"></i> Servizi
         </button>
-        <ul class="dropdown-menu">
-          <li><a class="dropdown-item" ><span class="badge text-bg-secondary">Secondary</span></a></li>
-        </ul>
+        <div class="dropdown-menu service-box">
+          <div class="d-flex flex-wrap px-2">
+            <input type="checkbox" class="btn-check" id="btn-check-outlined" autocomplete="off">
+            <label v-for="service in store.services" :key="service" class="btn btn-outline-primary m-2" for="btn-check-outlined">{{ service.name }}</label><br>
+          </div>
+          
+        </div>
       </div>
 
     </div>
@@ -99,6 +231,18 @@ import Results from '../components/Results.vue';
 
   .dropdown{
     margin-right: 20px;
+
+    .service-box{
+      width: 350px;
+    }
+
+    .dropdown-item{
+      padding: 0px;
+    }
+    .num{
+      padding: 10px 20px;
+    }
+    
   }
 }
 
