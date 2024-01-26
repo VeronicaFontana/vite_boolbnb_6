b9ffbaf1d1f65534732c88837e430aa30554bcd7
alessandrodaniello105 <kanymsevement@gmail.com>
2024-01-26 18:06:57 +0100

    [fix] filtered search by services; [test] Loader in AdvanceSearch

--- src/pages/AdvanceSearch.vue
+++ src/pages/AdvanceSearch.vue
@@ -3,6 +3,7 @@ import {store} from '../data/store';
 import Results from '../components/Results.vue';
 import Dropdown from '../components/partials/Dropdown.vue';
 import axios from 'axios';
+import Loader from '../components/Loader.vue';
 
 
   export default {
@@ -12,33 +13,70 @@ import axios from 'axios';
       store,
       axios,
       selectedServices: [], 
-      slider: 20000
+      slider: 20000,
+      isLoaded: false
     }
   },
   components:{
     Results,
     Dropdown,
+    Loader,
   },
   methods:{
     selectAndSearch({ value, category }) {
+      console.log('value:', value, 'cat.', category)
       store.selectedValues[category] = value;
+      console.log(store.selectedValues);
+      this.getFilteredApartments();
     },
+
+    executeQuery(name) {
+
+      let servicesArray = store.selectedValues.services;
+      
+      let index = servicesArray.indexOf(name);
+
+      // if(servicesArray.length == 0) {
+      //   store.selectedValues.services.push(name);
+      // }
+
+      if (index > -1) {
+        servicesArray.splice(index, 1);
+      } else {
+        store.selectedValues.services.push(name);
+      }
+
+      console.log(servicesArray)
+      this.getFilteredApartments();
+
+    },
+
     getFilteredApartments(){
-      axios.defaults.baseURL = 'http://127.0.0.1:8000';
-      axios.get(store.apiFilter, { 
+      store.isLoaded = false;
+
+      // axios.defaults.baseURL = 'http://127.0.0.1:8000';
+      axios.get(store.apiTest, { 
         params:{ 
-          results: JSON.stringify(store.results),
+          // results: JSON.stringify(store.results),
+          radius: this.slider,
+          lonA: store.lonA,
+          latA: store.latA,
           services: store.selectedValues.services,
           rooms: store.selectedValues.rooms,
           beds: store.selectedValues.beds,
         }
         })
         .then(response => {
-          store.filteredApartments = response.data.filteredApartmentsWithDistance;
-          console.log("Response from getFilteredApartments:", response.data);
-          store.filteredApartments.sort(function(a, b) {
-            return a.distanza - b.distanza;
-          });
+          console.log(response.data.apartments)
+          console.log("Response from results:", response.data);
+          store.results = response.data.apartments;
+          store.isLoaded = true;
+          if(response.data.servicesQuery.length > 0) {
+            store.servicesApartments = response.data.servicesQuery
+          }
+          // store.results.sort(function(a, b) {
+          //   return a.distance - b.distance;
+          // });
 
         })
         .catch(error => {
@@ -47,7 +85,9 @@ import axios from 'axios';
     },
 
     getApiNostra(){
-      axios.get(store.apiNostra, {
+      store.isLoaded = false;
+
+      axios.get(store.apiTest, {
         params:{ 
           radius: this.slider,
           lonA: store.lonA,
@@ -56,7 +96,8 @@ import axios from 'axios';
       })
     .then(res =>{
       console.log(res.data);
-      store.results = res.data
+      store.results = res.data.apartments;
+      store.isLoaded = true;
       // console.log( store.results )
     })
   },
@@ -77,7 +118,7 @@ import axios from 'axios';
       <!-- STANZE -->
       <div class="dropdown">
         <button class="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
-          <i class="fa-solid fa-person-shelter"></i> Stanze
+          <i class="fa-solid fa-person-shelter"></i> Stanze:
           <span>{{ store.selectedValues.rooms }}</span>
         </button>
         <Dropdown category="rooms" @dropdown-selected="selectAndSearch" />
@@ -86,16 +127,20 @@ import axios from 'axios';
       <!-- CAMERE -->
       <div class="dropdown">
         <button class="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
-          <i class="fa-solid fa-bed"></i> Camere da letto
+          <i class="fa-solid fa-bed"></i> Letti:
           <span>{{ store.selectedValues.beds }}</span>
         </button>
         <Dropdown category="beds" @dropdown-selected="selectAndSearch" />
       </div>
 
       <!-- SERVIZI -->
-      <button type="button" class="btn btn-secondary" data-bs-toggle="modal" data-bs-target="#exampleModal">
-        <i class="fa-solid fa-bell-concierge"></i> Servizi
-      </button>
+
+      <div class="dropdown">
+        <button type="button" class="btn btn-secondary dropdown-toggle" data-bs-toggle="modal" data-bs-target="#exampleModal">
+          <i class="fa-solid fa-bell-concierge"></i> Servizi
+        </button>
+      </div>
+
       <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
         <div class="modal-dialog modal-dialog-centered">
           <div class="modal-content">
@@ -105,8 +150,8 @@ import axios from 'axios';
             </div>
             <div class="modal-body">
               <div v-for="(service, index) in store.services" :key="index">
-                <input type="checkbox" :id="'btn-check-outlined-' + index" class="btn-check" autocomplete="off" :value="service.name" v-model="store.selectedValues.services"/>
-                <label :for="'btn-check-outlined-' + index" class="btn btn-outline-primary m-2" >{{ service.name }}</label> <br />
+                <input type="checkbox" :id="'btn-check-outlined-' + index" class="btn-check" autocomplete="off" :value="service.name" />
+                <label :for="'btn-check-outlined-' + index" class="btn btn-outline-primary m-2" category="services" @click="executeQuery(service.name)" >{{ service.name }}</label> <br />
               </div>
             </div>
           </div>
@@ -116,12 +161,11 @@ import axios from 'axios';
       <div class="radius-slider">
         <input type="range" min="2000" max="100000" step="10000" name="radius" id="radius" value="20000" v-model="slider" @change="getApiNostra()">
       </div>
-
-      <button type="button" class="btn btn-success ms-3" @click="getFilteredApartments()">Filtra i risultati</button>
     </div>
   </section>
 
-    <Results />
+
+  <Results />
 
 </template>
 
