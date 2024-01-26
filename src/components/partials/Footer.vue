8d83b60bfb3acadc9fb61c889bc69e347ed70580
vincenzoimmordino92 <vincenzoimmordino92@gmail.com>
2024-01-26 19:37:03 +0100

    [edit] Footer responsive

--- src/components/partials/Footer.vue
+++ src/components/partials/Footer.vue
@@ -13,27 +13,29 @@
 
 
 <template>
-<footer class="">
+<footer>
   <div class="container">
-    <div class="row">
-      <div class="col-3 one">
+    <div class="row flex-column align-content-center flex-sm-row">
+
+      <div class="col-sm-3 d-sm-flex flex-sm-row col-fluid  one">
         <i class="fa-brands fa-cc-visa"></i>
         <i class="fa-brands fa-cc-mastercard"></i>
         <i class="fa-brands fa-paypal"></i>
         <i class="fa-brands fa-cc-apple-pay"></i>
         <i class="fa-solid fa-lock"></i>
       </div>
-      <div class="col-9 two">
+      <div class="col-fluid col-sm-9 d-sm-flex flex-sm-row two">
         <p>BoolB&B.it rispetta lo standard PCI-DSS per i servizi di Prenotazione Alberghiera / Accomodation Reservation - Payment Card Industry Data Security Standard v.3.2, Service Provider Level 2</p>
       </div>
     </div>
-    <div class="row">
-      <div class="col-9 two">
+
+    <div class="row row flex-column align-content-center flex-sm-row">
+      <div class="col-fluid col-sm-9 d-sm-flex flex-sm-row two">
         <p>L'accesso o l'utilizzo di questo sito è subordinato all'accettazione dei Termini   del servizio, Informativa sulla Privacy e Cookie Policy
           Realizzazione:  Classe #105 Team-6 - P.Iva 6516516516
         </p>
       </div>
-      <div class="col-3 one">
+      <div class="col-sm-3 d-sm-flex flex-sm-row col-fluid one">
         <i class="fa-brands fa-facebook-f"></i>
         <i class="fa-brands fa-x-twitter"></i>
         <i class="fa-brands fa-instagram"></i>
@@ -41,7 +43,7 @@
         <i class="fa-brands fa-youtube"></i>
       </div>
     </div>
-  </div>
+  </div> 
   
 </footer>
 </template>
@@ -49,8 +51,6 @@
 
 <style lang="scss"  scoped>
 footer{
-  height: 250px;
-  width: 100vw;
   background-color: rgb(48, 47, 47);
   color: white;
   .row{
@@ -61,19 +61,16 @@ footer{
     .one{
       display: flex;
       align-items: center;
-      justify-content: center;
-      justify-content: space-around;
-    // background-color: rgb(172, 70, 70);
+      justify-content: space-evenly;
     i{
-      font-size: 1.9rem;
+      font-size: 2.1rem;
     }
     }
     .two{
-    // background-color: rgb(156, 98, 22);
-    padding-top:10px;
+      text-align: center;
+      padding: 10px;
     }
   }
 }
-
 
 </style>
