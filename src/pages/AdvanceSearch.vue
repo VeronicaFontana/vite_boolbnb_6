efc5136588131c1178272bb7b228d5d630a99114
vincenzoimmordino92 <vincenzoimmordino92@gmail.com>
2024-01-29 14:19:33 +0100

    [edit] Responsive of AdvanceSearch

--- src/pages/AdvanceSearch.vue
+++ src/pages/AdvanceSearch.vue
@@ -116,12 +116,27 @@ import Loader from '../components/Loader.vue';
 
 
 <template>
-  
+  <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    
+    <div class="modal-dialog modal-fullscreen-sm-down">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h1 class="modal-title fs-5" id="exampleModalLabel">Servizi disponibili</h1>
+          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+        </div>
+        <div class="modal-body">
+          <div v-for="(service, index) in store.services" :key="index">
+            <input type="checkbox" :id="'btn-check-outlined-' + index" class="btn-check" autocomplete="off" :value="service.name" />
+            <label :for="'btn-check-outlined-' + index" class="btn btn-outline-primary m-2" category="services" @click="executeQuery(service.name)" >{{ service.name }}</label> <br />
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
   <section class="filter-box">
     <div class="ms-2 h-100 d-flex align-items-center">
-      <SearchBar class="d-sm-none w-75"/>
 
-      
+      <SearchBar :input_id="'input_search_mobile'" :input_list="'address-list_mobile'" class="d-sm-none w-75"/>
 
       <div class="d-none d-sm-flex d-flex align-items-center">
         <!-- STANZE -->
@@ -132,6 +147,7 @@ import Loader from '../components/Loader.vue';
           </button>
           <Dropdown category="rooms" @dropdown-selected="selectAndSearch" />
         </div>
+        <!--FINE STANZE -->
 
         <!-- CAMERE -->
         <div class="dropdown">
@@ -141,46 +157,80 @@ import Loader from '../components/Loader.vue';
           </button>
           <Dropdown category="beds" @dropdown-selected="selectAndSearch" />
         </div>
+        <!-- FINE CAMERE -->
 
         <!-- SERVIZI -->
-
         <div class="dropdown">
           <button type="button" class="btn btn-secondary dropdown-toggle" data-bs-toggle="modal" data-bs-target="#exampleModal">
             <i class="fa-solid fa-bell-concierge"></i> Servizi
           </button>
         </div>
 
-        <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-          
-          <div class="modal-dialog modal-dialog-centered">
-            <div class="modal-content">
-              <div class="modal-header">
-                <h1 class="modal-title fs-5" id="exampleModalLabel">Servizi disponibili</h1>
-                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-              </div>
-              <div class="modal-body d-flex flex-wrap">
-                <div v-for="(service, index) in store.services" :key="index">
-                  <input type="checkbox" :id="'btn-check-outlined-' + index" class="btn-check" autocomplete="off" :value="service.name" />
-                  <label :for="'btn-check-outlined-' + index" class="btn btn-outline-primary m-2" category="services" @click="executeQuery(service.name)" >{{ service.name }}</label> <br />
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
+        
+        <!--FINE SERVIZI -->
 
+        <!--RAGGIO -->
         <div class="radius-slider">
           <input type="range" min="2000" max="100000" step="10000" name="radius" id="radius" value="20000" v-model="slider" @change="getApiNostra()">
         </div>
+        <!--FINE RAGGIO -->
+        
       </div>
     </div>
   </section>
 
+  <section class="filter-box-responsive d-sm-none">
+    <div class="ms-2 h-100 d-flex align-items-center">
+
+      <div class=" d-flex w-100 justify-content-evenly align-items-center">
+        <!-- STANZE -->
+        <div class="dropdown">
+          <button class="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+            <i class="fa-solid fa-person-shelter"></i> Stanze:
+            <span>{{ store.selectedValues.rooms }}</span>
+          </button>
+          <Dropdown category="rooms" @dropdown-selected="selectAndSearch" />
+        </div>
+        <!--FINE STANZE -->
+
+        <!-- CAMERE -->
+        <div class="dropdown">
+          <button class="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+            <i class="fa-solid fa-bed"></i> Letti:
+            <span>{{ store.selectedValues.beds }}</span>
+          </button>
+          <Dropdown category="beds" @dropdown-selected="selectAndSearch" />
+        </div>
+        <!-- FINE CAMERE -->
+
+        <!-- SERVIZI -->
+        <div class="dropdown">
+          <!-- BUTTON DEL MODAL RESPONSIVE CHA STA FUORI DALLA SECTION -->
+          <button type="button" class="btn btn-secondary dropdown-toggle" data-bs-toggle="modal" data-bs-target="#exampleModal">
+            <i class="fa-solid fa-bell-concierge"></i> Servizi
+          </button>
+        </div>
+        <!--FINE SERVIZI -->
+
+
+        <!--RAGGIO -->
+        <div class="radius-slider">
+          <input type="range" min="2000" max="100000" step="10000" name="radius" id="radius" value="20000" v-model="slider" @change="getApiNostra()">
+        </div>
+        <!--FINE RAGGIO -->
+        
+      </div>
+    </div>
+  </section>
 
   <Results />
 
 </template>
 
 <style lang="scss" scoped>
+
+@use '../scss/partials/palette' as *;
+
 .filter-box{
   width: 100vw;
   margin-top: 90px;
@@ -190,5 +240,11 @@ import Loader from '../components/Loader.vue';
   .dropdown{
     margin-right: 20px;
   }
+}
+.filter-box-responsive{
+  width: 100vw;
+  height: 60px;
+  background-color: lightgrey;
+
 }
 </style>
